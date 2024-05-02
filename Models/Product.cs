6d38294c570f2c364203b990502e56ac98f210e0
Lívia <livia.do.nascimento@avanade.com>
2024-05-02 13:53:29 -0300

    feat: Model de produtos

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashDash.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int CategoryID { get; set;}
+        public Category Category { get; set; }
+        [Required]
+        public int PriceInCents { get; set; }
+        public int InventoryCount { get; set; } 
+
+    }
+}
