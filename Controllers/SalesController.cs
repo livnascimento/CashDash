e10d9d430cfeb75a6f74378d54f7f4fa5fa9399f
Lívia <livia.do.nascimento@avanade.com>
2024-05-02 13:53:06 -0300

    feat: CRUD de Vendas

---
+++ Controllers/SalesController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CashDash.Data;
+using CashDash.Models;
+
+namespace CashDash.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly DBContext _context;
+
+        public SalesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sales
+        public async Task<IActionResult> Index()
+        {
+            var dBContext = _context.Sale.Include(s => s.Customer).Include(s => s.Employee).Include(s => s.Product);
+            return View(await dBContext.ToListAsync());
+        }
+
+        // GET: Sales/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sale
+                .Include(s => s.Customer)
+                .Include(s => s.Employee)
+                .Include(s => s.Product)
+                .FirstOrDefaultAsync(m => m.SaleID == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
+        // GET: Sales/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerID"] = new SelectList(_context.Customer, "CustomerID", "CustomerID");
+            ViewData["EmployeeID"] = new SelectList(_context.Employee, "EmployeeId", "EmployeeId");
+            ViewData["ProductID"] = new SelectList(_context.Product, "ProductID", "Description");
+            return View();
+        }
+
+        // POST: Sales/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SaleID,SaleDateTime,ProductID,CustomerID,EmployeeID")] Sale sale)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sale);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customer, "CustomerID", "CustomerID", sale.CustomerID);
+            ViewData["EmployeeID"] = new SelectList(_context.Employee, "EmployeeId", "EmployeeId", sale.EmployeeID);
+            ViewData["ProductID"] = new SelectList(_context.Product, "ProductID", "Description", sale.ProductID);
+            return View(sale);
+        }
+
+        // GET: Sales/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sale.FindAsync(id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customer, "CustomerID", "CustomerID", sale.CustomerID);
+            ViewData["EmployeeID"] = new SelectList(_context.Employee, "EmployeeId", "EmployeeId", sale.EmployeeID);
+            ViewData["ProductID"] = new SelectList(_context.Product, "ProductID", "Description", sale.ProductID);
+            return View(sale);
+        }
+
+        // POST: Sales/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SaleID,SaleDateTime,ProductID,CustomerID,EmployeeID")] Sale sale)
+        {
+            if (id != sale.SaleID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sale);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SaleExists(sale.SaleID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customer, "CustomerID", "CustomerID", sale.CustomerID);
+            ViewData["EmployeeID"] = new SelectList(_context.Employee, "EmployeeId", "EmployeeId", sale.EmployeeID);
+            ViewData["ProductID"] = new SelectList(_context.Product, "ProductID", "Description", sale.ProductID);
+            return View(sale);
+        }
+
+        // GET: Sales/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sale = await _context.Sale
+                .Include(s => s.Customer)
+                .Include(s => s.Employee)
+                .Include(s => s.Product)
+                .FirstOrDefaultAsync(m => m.SaleID == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
+        }
+
+        // POST: Sales/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sale = await _context.Sale.FindAsync(id);
+            if (sale != null)
+            {
+                _context.Sale.Remove(sale);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SaleExists(int id)
+        {
+            return _context.Sale.Any(e => e.SaleID == id);
+        }
+    }
+}
---
+++ Models/Sale.cs
@@ -0,0 +1,14 @@
+﻿namespace CashDash.Models
+{
+    public class Sale
+    {
+        public int SaleID { get; set; }
+        public DateTime SaleDateTime { get; set; } = DateTime.Now;
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+        public int EmployeeID { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
