23ddaec2ec3ee82cb88eb53bfd2943e9ba7b8af7
Lívia <livia.do.nascimento@avanade.com>
2024-05-02 13:51:39 -0300

    feat: CRUD de clientes

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashDash.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "O código postal deve ter exatamente 8 caracteres.")]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly RegisterDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
