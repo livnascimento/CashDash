1fa1d53299dc34cbbba142d54d8c7a266f7f96bc
Lívia <livia.do.nascimento@avanade.com>
2024-05-03 11:11:28 -0300

    refactor: adicionando dinamicidade aos indicadores da home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 using CashDash.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using CashDash.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CashDash.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var sales = _context.Sale
+                        .OrderByDescending(sale => sale.SaleID)
+                        .Include(sale => sale.Customer)
+                        .Include(sale => sale.Product)
+                        .Include(sale => sale.Employee)
+                        .Take(10)
+                        .ToList();
+            ViewBag.EmployeesCount = _context.Employee.Count();
+            ViewBag.SalesCount = _context.Sale.Count();
+            ViewBag.CustomerCount = _context.Customer.Count();
+            ViewBag.ProductCount = _context.Product.Count();
+            return View(sales);
         }
 
         public IActionResult Privacy()
